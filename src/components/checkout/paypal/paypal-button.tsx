208fe734b671c6b254d6f85a6d2236492980546f
TanLeJS <tanlengoc2000@gmail.com>
2024-09-06 23:20:45 -0400

    paypal integration

--- src/components/checkout/paypal/paypal-button.tsx
+++ src/components/checkout/paypal/paypal-button.tsx
@@ -11,29 +11,57 @@ interface PayPalPaymentButtonProps {
 }
 
 const PaypalPaymentButton: React.FC<PayPalPaymentButtonProps> = ({ amount, paypalClientId, onSuccess, onError }) => {
+    const createOrder = () => {
+        return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/paypal/orders`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // use the "body" param to optionally pass additional order information
+            // like product ids and quantities
+            body: JSON.stringify({
+                cart: [
+                    {
+                        sku: "1blwyeo8",
+                        quantity: 2,
+                    },
+                ],
+            }),
+        })
+            .then((response) => response.json())
+            .then((order) => {
+                // Your code here after create the order
+                return order.id;
+            });
+    }
+
+
+    const onApprove = (data) => {
+        return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/paypal/orders/capture`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                orderID: data.orderID
+            })
+        })
+            .then((response) => response.json())
+            .then((orderData) => {
+                const name = orderData.payer.name.given_name;
+                alert(`Transaction completed by ${name}`);
+            });
+    }
+
+
+
     return (
         <Box style={{ maxWidth: '750px', minHeight: '200px' }}>
             {paypalClientId && (
                 <PayPalScriptProvider options={{ clientId: paypalClientId, currency: 'USD' }}>
                     <PayPalButtons
-                        style={{ layout: 'vertical' }}
-                        createOrder={(data, actions) => {
-                            return actions.order.create({
-                                purchase_units: [{
-                                    amount: {
-                                        value: amount.toString(),
-                                    },
-                                }],
-                            });
-                        }}
-                        onApprove={(data, actions) => {
-                            return actions.order.capture().then((details) => {
-                                onSuccess(); // Call the success handler
-                            });
-                        }}
-                        onError={(err) => {
-                            onError(err); // Call the error handler
-                        }}
+                        createOrder={createOrder}
+                        onApprove={(data) => onApprove(data)}
                     />
                 </PayPalScriptProvider>
             )}
